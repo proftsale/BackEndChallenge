ae6a3af9641fb1562f00d97221f28cfc40d1da40
Hugo Fusinato <hugohvf@hotmail.comDAA>
2020-09-04 19:14:41 -0300

    2a Quest - Numero Nacisista
    BackEndChallenge - Proft

---
+++ src/controllers/NumeroNarcisistaController.js
@@ -0,0 +1,21 @@
+module.exports = {
+  async index(request, response) {
+    const { numero } = request.params;
+
+    tamanhoNumero = numero.length;
+
+    digitos = [...numero]
+
+    digitosElevados = digitos.map((num) => Math.pow(num, tamanhoNumero))
+
+    somaDigitos = digitosElevados.reduce((num1, num2) => num1 + num2)
+
+    isNarcisista = somaDigitos == numero;
+
+    successMessage = {
+      result: isNarcisista,
+    };
+
+    return response.json(successMessage);
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 const express = require ('express');
 
-const PingController = require('./controllers/PingPongController')
+const PingController = require('./controllers/PingPongController');
+const NumeroNarcisista = require('./controllers/NumeroNarcisistaController');
 
 const routes = express.Router();
 
 routes.get('/v1/ping', PingController.index);
+
+routes.get('/v1/narcisista/:numero', NumeroNarcisista.index);
 
 module.exports = routes; 
 
